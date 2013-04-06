2a216173a24d2280ede240fc965f6d4e1afe9482
Derek Rodriguez <drod2169@gmail.com>
2013-04-06 16:02:41 -0400

    Add user relationship, delete function

--- grails-app/domain/testapp/Inventory.groovy
+++ grails-app/domain/testapp/Inventory.groovy
@@ -1,13 +1,18 @@
 package testapp
 
 class Inventory {
+	static belongsTo = [admin:User]
 	static hasMany = [albums:Album]
 	Inventory(){
 		albums = [""]
 	}
 
 	def addAlbum(Album album) {
-	
+
+	}
+
+	def deleteAlbum(Album album) {
+	album.delete()
 	}
 
     static constraints = {
