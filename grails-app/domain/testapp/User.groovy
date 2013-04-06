eaca2fdcbf46fda963c329819efa582876fd06d2
Derek Rodriguez <drod2169@gmail.com>
2013-04-06 15:05:52 -0400

    Add relationship to shopping cart, more values for user login/registration

--- grails-app/domain/testapp/User.groovy
+++ grails-app/domain/testapp/User.groovy
@@ -1,11 +1,20 @@
 package testapp
 
 class User {
+	static hasOne = [shoppingcart:ShoppingCart]
+
+	String login
+	String password
 	String firstName
 	String lastName
+	String email
 
     static constraints = {
+	login(unique:true,length:5..15)
+	password(matches:/[\w\d]+/, length:6..12)
+	email(email:true)
 	firstName(blank:false)
 	lastName(blank:false)
     }
+
 }
