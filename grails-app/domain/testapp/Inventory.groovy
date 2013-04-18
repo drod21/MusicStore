78fbb683cdeca73853576eeba0846dc12a1b21f5
Derek Rodriguez <drod2169@gmail.com>
2013-04-18 15:11:35 -0400

    Remove delete function, change belongsTo dependency

--- grails-app/domain/testapp/Inventory.groovy
+++ grails-app/domain/testapp/Inventory.groovy
@@ -1,18 +1,18 @@
 package testapp
 
 class Inventory {
-	static belongsTo = [admin:User]
+	static belongsTo = [adminName:Admin]
 	static hasMany = [albums:Album]
+
+	
 	Inventory(){
 		albums = [""]
 	}
 
 	def addAlbum(Album album) {
-
 	}
 
 	def deleteAlbum(Album album) {
-	album.delete()
 	}
 
     static constraints = {
