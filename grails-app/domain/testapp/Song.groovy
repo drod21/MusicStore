d2fae3129f52cd00fbd181e62d78950d8c880169
Derek Rodriguez <drod2169@gmail.com>
2013-04-19 09:10:10 -0400

    Clean up. Add genre string.

--- grails-app/domain/testapp/Song.groovy
+++ grails-app/domain/testapp/Song.groovy
@@ -1,15 +1,17 @@
 package testapp
 
 class Song {
-	String title
-	String artist
-	Integer duration
 
-	static belongsTo = Album
+    String title
+    String genre
+    Integer duration
+    Album album
+    Artist artist
 
     static constraints = {
 	title(blank:false)
-	artist(blank:false)
 	duration(min:1)
 	}
+
+    String toString() { title }
 }
