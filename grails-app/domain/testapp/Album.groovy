10f3aa8bd9abe7f494cfc7cff435f82fbea9c455
Derek Rodriguez <drod2169@gmail.com>
2013-04-19 09:09:03 -0400

    Add genre string, list the songs, add toString for title

--- grails-app/domain/testapp/Album.groovy
+++ grails-app/domain/testapp/Album.groovy
@@ -2,11 +2,15 @@ package testapp
 
 class Album {
 	String title
+    String genre
 
+
+    List songs
 	static hasMany = [songs:Song]
 	static belongsTo = [artist:Artist]
 
     static constraints = {
 	title(blank:false)
     }
+    String toString() { title }
 }
