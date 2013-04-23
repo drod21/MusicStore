1b034a10011d8b40882e93e77340a3570530a798
Derek Rodriguez <drod2169@gmail.com>
2013-04-23 09:12:05 -0400

    Admin should not test for delete/add functions

--- test/unit/testapp/AdminTests.groovy
+++ test/unit/testapp/AdminTests.groovy
@@ -12,23 +12,9 @@ import org.junit.*
 class AdminTests {
 
     void testUserAdmin() {
-	def adminName = new User(login:'AdminTest',,
+	def adminName = new User(login:'AdminTest',
 			email:'admin@musicstore.com')
 	assert adminName.getLogin().equals('AdminTest'), getEmail().equals('admin@musicstore.com')
-}
-	void testAddAlbum() {
-	Inventory inventory = new Inventory()
-	inventory.addAlbum(new Album(title:'Me Against The World'))
-	Integer count = inventory.albums.size()
-	assert count == 1  
-}
-	void testDeleteAlbum() {
-	Inventory inventory = new Inventory()
-	inventory.addAlbum(new Album(title:'Me Against The World'))
-	def album = Album.get(1)
-	album.delete(flush: true)
-	Integer count = inventory.albums.size()
-	assert count == 0
 }
 
 }
