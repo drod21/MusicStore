9dba7b7def5f93d5b80b9c39007f7403ad0b0eef
Derek Rodriguez <drod2169@gmail.com>
2013-04-23 09:02:58 -0400

    Add the controller command from the videos. Start to add code to display album

--- grails-app/controllers/testapp/AlbumController.groovy
+++ grails-app/controllers/testapp/AlbumController.groovy
@@ -1,15 +1,46 @@
 package testapp
 
 class AlbumController {
-	def save(params){
-	    if(Album.save()) {
-		album.properties = params
-		album.save()
-		redirect(action: "show", id:album.id)
-		} else {
-		render(view: "create", model:[cmd:cmd])
-	}
+
+    def save(AlbumCreateCommand cmd) {
+        if(cmd.validate()) {
+            def album = cmd.createAlbum()
+            album.save()
+            redirect(action: "show", id:album, id)
+        } else {
+            render view: "create", model:[cmd:cmd]
+        }
+    }
+    def display = {
+        def album = Album.get(params.id)
+        if(album) {
+            def artist = album.artist
+            render(template:"album", model:[artist:artist, album:album])
+        }
+        else {
+            render "Album not found."
+        }
     }
 }
+    class AlbumCreateCommand {
+        String artist
+        String title
+        List songs = []
+        List durations = []
 
+        static constraints = {
+            artist blank: false
+            title blank: false
+            songs minSize: 1
+        }
+
+        Album createAlbum() {
+            def artist = Artist.findByName(artist) ?: new Artist(name:artist)
+            def album = new Album(title:title)
+            songs.eachWithIndex { songTitle, i ->
+            album.addToSongs(title:songTitle, duration:durations[i])
+            }
+            return album
+        }
+}
 
