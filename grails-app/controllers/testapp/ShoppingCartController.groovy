6c410a24cc26bb54e79959f21971c7cd2f5942d4
Derek Rodriguez <drod2169@gmail.com>
2013-04-27 16:07:04 -0400

    Update shopping cart controller and the add/remove methods

--- grails-app/controllers/testapp/ShoppingCartController.groovy
+++ grails-app/controllers/testapp/ShoppingCartController.groovy
@@ -2,5 +2,35 @@ package testapp
 
 class ShoppingCartController {
 
-    def index() { }
+    def addToCart = {
+
+    def album = Album.get(params.id)
+
+    if (album == null) {
+        flash.error = "Could not add album to cart! Could find product with id [${params.id}]."
+    }
+
+    AlbumInstance.addToAlbums(album)
+
+    AlbumInstance.save(flush:true)
+
+
+    redirect(action:'cart')
+}
+
+    def removeFromCart() {
+        def cartItems =  {
+            if (ShoppingCart == null) {
+                flash.error = "You have no albums in your cart."
+            } else {
+                println("Select an album to remove:")
+                cart.getProperties(params.id)
+                cart.removeFromCart(album)
+            }
+
+        }
+
+
+    }
+
 }
