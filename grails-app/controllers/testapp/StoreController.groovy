bb25bc2ba187522a3839ec201c9485e2d1f047d9
Derek Rodriguez <drod2169@gmail.com>
2013-04-29 19:14:14 -0400

    Update shop method in store controller

--- grails-app/controllers/testapp/StoreController.groovy
+++ grails-app/controllers/testapp/StoreController.groovy
@@ -9,6 +9,15 @@ class StoreController {
 		[albums:albumList.sort()]
     }
 	
-	def shop() {
+	def shop = {
+		def genreList = Album.withCriteria {
+			projections {
+				distinct "genre"
+			}
+		}
+		[genres:genreList.sort(),
+			top5Albums:Album.list(max:5, sort:"dateCreated", order:"desc"),
+			top5Songs:Song.list(max:5, sort:"dateCreated", order:"desc"),
+			top5Artists:Artist.list(max:5, sort:"dateCreated", order:"desc")] 
 	}
 }
