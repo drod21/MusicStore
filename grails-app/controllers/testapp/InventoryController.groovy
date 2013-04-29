6e5704e80730baccc71530b437b4203af5ccf6c8
Derek Rodriguez <drod2169@gmail.com>
2013-04-29 09:07:00 -0400

    Clean up inventory controller code, fix some functionality

--- grails-app/controllers/testapp/InventoryController.groovy
+++ grails-app/controllers/testapp/InventoryController.groovy
@@ -1,6 +1,39 @@
 package testapp
 
+import testapp.AlbumController
+
 class InventoryController {
 
-    def index() { }
+    def index() {
+        redirect(action: "list")
+    }
+
+    def addToInventory() {
+        if(cmd.validate()) {
+            def album = cmd.createAlbum()
+            album.save()
+            redirect(action: "show", id:album, id)
+        } else {
+            render view: "create", model:[cmd:cmd]
+        }
+    }
+
+    private static def removeFromInventory(Album album) {
+            album.delete()
+                       .removeFromInventory album
+    }
+
+    def updateAlbum() {
+        def album = Album.get(id)
+        album = ""
+        album.save()
+    }
+
+    def list() {
+        def albums = Album.id()
+        albums.each {
+        println it['title']
+        }
+        render view: "list", model:[title:Album]
+    }
 }
