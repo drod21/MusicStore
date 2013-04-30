69fd75a81256921cb9a674a7c5532607445fcecb
Derek Rodriguez <drod2169@gmail.com>
2013-04-30 08:35:28 -0400

    Updates to shopping cart controller

--- grails-app/controllers/testapp/ShoppingCartController.groovy
+++ grails-app/controllers/testapp/ShoppingCartController.groovy
@@ -2,21 +2,20 @@ package testapp
 
 class ShoppingCartController {
 
-static scaffold = true
 
     def index() {
         redirect(action: "list")
     }
 
     def addToCart() {
-    def album = Album.get(params.id)
+        def album = Album.get(params.id)
         if (album == null) {
-        flash.error = "Could not add album to cart! Could find product with id [${params.id}]."
+            flash.error = "Could not add album to cart! Could find product with id [${params.id}]."
+        }
+        album.addToCart(album)
+        album.save(flush: true)
+        redirect(action:'cart')
     }
-    album.addToCart(album)
-    album.save(flush: true)
-    redirect(action:'cart')
-}
 
     def removeFromCart() {
         def cartItems =  {
@@ -26,7 +25,7 @@ static scaffold = true
                 def cart = ShoppingCart.getAll()
                 def album = cart.list()
                 cart.each {
-                println it.getAt('album.title')
+                    println it.getAt('album.title')
                 }
                 println("Select an album to remove:")
                 cart.removeFromCart(album)
@@ -43,11 +42,12 @@ static scaffold = true
                 def cart = ShoppingCart.getAll()
                 def album = cart.list()
                 cart.each {
-                println it.getAt('album.title')
+                    println it.getAt('album.title')
                 }
-        render view: "list", model:[title:Album]
-    }
+                render view: "list", model:[title:Album]
+            }
 
+        }
+    }
 }
-}
-}
+
