5e0cc2c5db8e573ccf0944e15e844e5fef03434c
Derek Rodriguez <drod2169@gmail.com>
2013-04-30 09:30:07 -0400

    Only allow admin to create songs

--- grails-app/controllers/testapp/SongController.groovy
+++ grails-app/controllers/testapp/SongController.groovy
@@ -2,4 +2,20 @@ package testapp
 
 class SongController {
 	static scaffold = true
+	
+	SongController() {
+	}
+
+	def index = {
+	}
+
+	def create = {
+	if (session.user?.isAdmin()) {
+	   def song = new Song()
+	   song.properties = params
+	   return [songInstance: song]
+	} else {
+	  redirect: "/store/index"
+	}
+     }
 }
